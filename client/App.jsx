51eda7e07c7ff0cf43fed50619044b9c8b8788e6
dasnyder3 <dasnyder3@gmail.com>
2021-01-12 20:03:42 -0500

    webpack configured

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    // console.log('in App component');
+    return <p>Hello World!</p>
+  }
+}
+
+export default App;
