a1c1c684fce81e1a81f2599dba3b6414a6e25ae6
dasnyder3 <dasnyder3@gmail.com>
2021-01-12 20:19:31 -0500

    dev environment working

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(3000);
+app.listen(3000); 
