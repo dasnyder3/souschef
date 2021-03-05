5c73324ce36fc6cdf7f6e481dfa376200b0eb073
dasnyder3 <dasnyder3@gmail.com>
2021-03-04 20:37:53 -0500

    fixed breaking issues

--- __tests__/test.js.test.js
+++
@@ -1,9 +0,0 @@
-import Ingredient from '../client/Components/Ingredient.jsx';
-import { render, fireEvent } from '@testing-library/react';
-import { build, fake } from 'test-data-bot';
-import '@testing-library/jest-dom/extend-expect'
-
-
-describe('', () => {
-  it('render to show amount of ingredient and scale it based on the scaler value', () => { const {} = render(<Ingredient amount={2}unit={tablespoon}originalName={cheese}scaler={1}/>); })
-});
