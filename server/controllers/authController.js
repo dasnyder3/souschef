b1aa342b6ce2e1a6fb56d2bcb4ea041d41268e4b
dasnyder3 <dasnyder3@gmail.com>
2021-05-10 16:34:44 -0400

    improved error logging

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,57 +15,66 @@ module.exports = {
           },
         })
       );
-    if (!user) {
-      const createUser = `
-          INSERT INTO users (google_id, display_name, given_name, family_name, picture, email)
-          VALUES ($1, $2, $3, $4, $5, $6)
-          RETURNING *
-        `;
-      const createUserParams = [
-        profile.id,
-        profile.displayName,
-        profile.name.givenName,
-        profile.name.familyName,
-        profile.photos[0].value,
-        profile.emails[0].value,
-      ];
-      user = await db
-        .query(createUser, createUserParams)
-        .then((data) => data.rows[0])
-        .catch((err) =>
-          next({
-            log: `findOrCreateUser: ERROR: ${err}`,
-            message: {
-              err: 'ERROR: Check server logs for details',
-            },
-          })
-        );
-    } else {
-      const updateUser = `
-          UPDATE users
-          SET display_name = $2, given_name = $3, family_name = $4, picture = $5, email = $6
-          WHERE google_id = $1
-          RETURNING *
-        `;
-      const updateUserParams = [
-        profile.id,
-        profile.displayName,
-        profile.name.givenName,
-        profile.name.familyName,
-        profile.photos[0].value,
-        profile.emails[0].value,
-      ];
-      user = await db
-        .query(updateUser, updateUserParams)
-        .then((data) => data.rows[0])
-        .catch((err) =>
-          next({
-            log: `findOrCreateUser: ERROR: ${err}`,
-            message: {
-              err: 'ERROR: Check server logs for details',
-            },
-          })
-        );
+    try {
+      if (!user) {
+        const createUser = `
+            INSERT INTO users (google_id, display_name, given_name, family_name, picture, email)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *
+          `;
+        const createUserParams = [
+          profile.id,
+          profile.displayName,
+          profile.name.givenName,
+          profile.name.familyName,
+          profile.photos[0].value,
+          profile.emails[0].value,
+        ];
+        user = await db
+          .query(createUser, createUserParams)
+          .then((data) => data.rows[0])
+          .catch((err) =>
+            next({
+              log: `findOrCreateUser: ERROR: ${err}`,
+              message: {
+                err: 'ERROR: Check server logs for details',
+              },
+            })
+          );
+      } else {
+        const updateUser = `
+            UPDATE users
+            SET display_name = $2, given_name = $3, family_name = $4, picture = $5, email = $6
+            WHERE google_id = $1
+            RETURNING *
+          `;
+        const updateUserParams = [
+          profile.id,
+          profile.displayName,
+          profile.name.givenName,
+          profile.name.familyName,
+          profile.photos[0].value,
+          profile.emails[0].value,
+        ];
+        user = await db
+          .query(updateUser, updateUserParams)
+          .then((data) => data.rows[0])
+          .catch((err) =>
+            next({
+              log: `findOrCreateUser: ERROR: ${err}`,
+              message: {
+                err: 'ERROR: Check server logs for details',
+              },
+            })
+          );
+      }
+    } catch (err) {
+      return next({
+        log: `findOrCreateUser: ERROR: ${err}`,
+        message: {
+          err: 'ERROR: Check server logs for details',
+        },
+      });
     }
   },
   checkUserLoggedIn: (req, res, next) => {
